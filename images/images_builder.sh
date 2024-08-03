e1b892b0f72c408ebedf264d03fb1da055f00af9
41k3m157 <elaazmielmahdi@gmail.com>
2024-08-03 04:36:26 +0100

    updating p2

---
+++ images/images_builder.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+bash ./routers/images_script.sh && bash ./hosts/images_script.sh 
+echo "Building Process Completed"
---
+++ images/routers/routers_builder.sh
@@ -0,0 +1,3 @@
+docker build --file=eelaazmi_router-1 --tag=eelaazmi_first_router . && echo -e "Router Frr First image is built"\
+
+docker build --file=eelaazmi_router-2 --tag=eelaazmi_seconde_router . && echo -e "Router Frr Seconde image is built"\
